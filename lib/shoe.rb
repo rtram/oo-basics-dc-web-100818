4bd86ce024782a5c4fd109bb8e7017496f3961a8
Robin Tram <robin.tram@gmail.com>
2018-10-10 13:56:38 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,18 +1,10 @@
 class Shoe
-  attr_accessor :color, :size, :material
+  attr_accessor :color, :size, :material, :condition
   attr_reader :brand
   
   
   def initialize(brand)
     @brand = brand
-  end 
-  
-  def condition=(condition)
-    @condition = condition
-  end 
-  
-  def condition
-    @condition
   end 
   
   def cobble 
